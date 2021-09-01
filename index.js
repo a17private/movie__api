ecdcde1680632f0ff2859a29dae9aac33e921024
a17labs <*a17matte@gmail.com*>
2021-09-01 16:05:25 +0100

    changes

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ app.get('/movies/Director/:Name', passport.authenticate('jwt', { session: false 
 
 // Register User
 
-app.post('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post('/users', (req, res) => {
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
