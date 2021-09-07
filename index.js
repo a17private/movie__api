aa0d27731460ad9527f581628b8d5c180c3c4779
a17labs <*a17matte@gmail.com*>
2021-09-07 12:40:53 +0100

    changes

--- index.js
+++ index.js
@@ -137,8 +137,7 @@ app.post('/users',  [
   check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
-  check('Email', 'Email does not appear to be valid').isEmail(),
-  check('Birthday', 'Birthday contains non numeric characters - not allowed.').isNumeric()
+  check('Email', 'Email does not appear to be valid').isEmail()
 ], (req, res) => {
    // check the validation object for errors
    let errors = validationResult(req);
