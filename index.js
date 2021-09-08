e202ea4cba1dc4fe6f02331bbf49a2fcf95a587c
a17labs <*a17matte@gmail.com*>
2021-09-08 13:58:06 +0100

    comment out env.PORT

--- index.js
+++ index.js
@@ -345,8 +345,12 @@ app.use(myLogger);
 
 
 // listen for requests
-const port = process.env.PORT || 8080;
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+}); 
+
+/*const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
-});
+});*/
 
