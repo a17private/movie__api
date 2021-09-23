04a7690159f49bdb82d7817099c01ebc4bce2d7d
a17labs <*a17matte@gmail.com*>
2021-09-23 21:24:06 +0100

    changes

--- index.js
+++ index.js
@@ -128,18 +128,21 @@ app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) =
 
 
 
-// Get one username
-
-app.get('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOne({ Username: req.params.username })
-  .then(user => {
-    res.status(201).json(user);
-  })
-  .catch(err => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
-});
+// Get a user by username
+app.get(
+  "/users/:Username",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 
 
