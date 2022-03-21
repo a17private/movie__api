4a26d690cf1987a40308f429f9086fa9940fa902
a17labs <*a17matte@gmail.com*>
2022-03-21 19:15:17 +0000

    changes new

--- index.js
+++ index.js
@@ -303,10 +303,10 @@ app.post('/users/:Username/movies/:MovieID', /*passport.authenticate('jwt', { se
 app.delete('/users/:Username/:MovieID', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.findOneAndUpdate(
   {Username: req.params.Username},
-  {$pull: {FavouriteMovies: req.params.MovieID}},
+  {$pull: {FavoriteMovies: req.params.MovieID}},
   {new: true}
   ).then((user) => {
-    res.status(200).json(user.FavouriteMovies);
+    res.status(200).json(user.FavoriteMovies);
   })
   .catch((error) => {
     console.error(error);
