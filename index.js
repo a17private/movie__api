66583af91387341dfd0f581dee5cef8f5749f4d9
a17labs <*a17matte@gmail.com*>
2022-03-21 18:43:05 +0000

    changes

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const Users = Models.User;
 
 const app = express();
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
-/*mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });*/
+/*mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnified8Topology: true });*/
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const cors = require('cors');
 app.use(cors());
 let auth = require('./auth')(app);
